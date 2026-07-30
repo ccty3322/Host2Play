dc8f5225b87f37c0119efb041687649fd652b516
ccty3322 <164542552+ccty3322@users.noreply.github.com>
2026-07-30 21:44:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://https://host2play.gratis/server/renew?i=6c8dab5e-625f-4a78-afa9-5c25b38ae065",
+    "https://host2play.gratis/server/renew?i=6c8dab5e-625f-4a78-afa9-5c25b38ae065",
     # 添加更多链接
 ]
 
